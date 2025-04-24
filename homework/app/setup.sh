00c02d0eea47a646a78df9e367839a921b86e47e
Danila Starikov <1132226531@pfur.ru>
2025-04-24 19:51:01 +0300

    chore(homework): add setup.sh script

---
+++ homework/app/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# create virtual enviroment
+python -m venv .venv
+. .venv/bin/activate
+pip install -r requirements.txt
+
+# setup .flask_env file
+python -c 'import secrets; print(secrets.token_hex())' > .flask_env
+
+# create static directory for images
+
+mkdir ./static
